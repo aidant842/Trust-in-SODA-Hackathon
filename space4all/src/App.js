3dc4f304365aa93947e6798dd723ada7729467f2
Aidan Thorpe <aidant842@gmail.com>
2021-09-10 20:22:57 +0100

    added react router, framer motion and changed nav slightly

--- space4all/src/App.js
+++ space4all/src/App.js
@@ -2,22 +2,40 @@
 import GlobalStyle from "./components/GlobalStyle";
 
 //Pages
-// import Page1 from "./pages/Page1";
+import Page1 from "./pages/Page1";
+import Page2 from "./pages/Page2";
+import Page3 from "./pages/Page3";
+import Page4 from "./pages/Page4";
+
+//Components
 import Nav from "./components/Nav";
 import Footer from "./components/Footer";
-import Container from "./components/UI/Container";
 
+// Router
+import { useLocation, Switch, Route } from "react-router-dom";
 
 function App() {
+    //Variables
+    const location = useLocation();
     return (
         <div className="App">
             <GlobalStyle />
             <Nav />
-            <Container>
-            <p>
-            Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
-            </p>
-            </Container>
+            <Switch location={location} key={location.pathname}>
+                {/* Add exact otherwise other urls that begin with a / will be rendered on that page */}
+                <Route path="/" exact>
+                    <Page1 />
+                </Route>
+                <Route path="/images" exact>
+                    <Page2 />
+                </Route>
+                <Route path="/checklist">
+                    <Page3 />
+                </Route>
+                <Route path="/resources" exact>
+                    <Page4 />
+                </Route>
+            </Switch>
             <Footer />
         </div>
     );
--- space4all/src/components/Nav.js
+++ space4all/src/components/Nav.js
@@ -1,55 +1,157 @@
-import styled from "styled-components"
+// Styled Components
+import styled from "styled-components";
 //Images
-import logo from "../static/images/logo.png"
+import logo from "../static/images/logo.png";
+// Router
+import { Link } from "react-router-dom";
+import { useLocation } from "react-router-dom";
+//Framer Motion
+import { motion } from "framer-motion";
 
 const Nav = () => {
+    const { pathname } = useLocation();
+    // variables
+
+    const red = "#e03f42";
+    const blue = "#62a3d1";
+
     return (
         <StyledNav>
+            <h1>
+                <Link id="logo" to="/">
+                    <img src={logo} alt="Space for All Logo" id="logo"></img>
+                </Link>
+            </h1>
             <ul>
                 <li>
-                    <a href="#"><img src={logo}></img></a>
+                    <Link
+                        to="/"
+                        style={{ color: pathname === "/" ? red : blue }}
+                    >
+                        Home
+                    </Link>
+                    <Line
+                        transition={{ duration: 0.75 }}
+                        initial={{ width: "0%" }}
+                        animate={{ width: pathname === "/" ? "50%" : "0%" }}
+                    />
                 </li>
                 <li>
-                    <a href="#">Home</a>
+                    <Link
+                        to="/images"
+                        style={{ color: pathname === "/images" ? red : blue }}
+                    >
+                        Images
+                    </Link>
+                    <Line
+                        transition={{ duration: 0.75 }}
+                        initial={{ width: "0%" }}
+                        animate={{
+                            width: pathname === "/images" ? "50%" : "0%",
+                        }}
+                    />
                 </li>
                 <li>
-                    <a href="#">Images</a>
+                    <Link
+                        to="/checklist"
+                        style={{
+                            color: pathname === "/checklist" ? red : blue,
+                        }}
+                    >
+                        Checklist
+                    </Link>
+                    <Line
+                        transition={{ duration: 0.75 }}
+                        initial={{ width: "0%" }}
+                        animate={{
+                            width: pathname === "/checklist" ? "50%" : "0%",
+                        }}
+                    />
                 </li>
                 <li>
-                    <a href="#">Checklist</a>
-                </li>
-                <li>
-                    <a href="#">Resources</a>
+                    <Link
+                        to="/resources"
+                        style={{
+                            color: pathname === "/resources" ? red : blue,
+                        }}
+                    >
+                        Resources
+                    </Link>
+                    <Line
+                        transition={{ duration: 0.75 }}
+                        initial={{ width: "0%" }}
+                        animate={{
+                            width: pathname === "/resources" ? "50%" : "0%",
+                        }}
+                    />
                 </li>
             </ul>
         </StyledNav>
     );
 };
+const StyledNav = styled.nav`
+    min-height: 10vh;
+    display: flex;
+    margin: auto;
+    margin-bottom: 1rem;
+    justify-content: space-between;
+    align-items: center;
+    padding: 1rem 10rem;
+    background-color: white;
+    box-shadow: 0 0 5px black;
+    position: sticky;
+    top: 0;
+    z-index: 100;
+    a {
+        color: var(--blue);
+        text-decoration: none;
+        :hover {
+            color: var(--red) !important;
+        }
+    }
 
-const StyledNav = styled.nav `
-    ul{
+    ul {
         display: flex;
-        background-color: white;
-        box-shadow: 0 0 5px black;
         list-style: none;
-        margin-bottom: 1rem;
-        li {
-            margin: auto;
-            img{
-                height: 50px;
-                width: 50px;
-            }
-            a{
-                cursor: pointer;
-                color: var(--blue);
-                /* text-shadow: 0 0 2px var(--d-blueish); */
-                text-decoration: unset;
-                :hover{
-                    font-size: 1.2rem;
-                    color: var(--red);
-                }
+    }
+
+    #logo {
+        height: 75px;
+        width: 75px;
+    }
+
+    li {
+        padding-left: 10rem;
+        position: relative;
+    }
+    @media (max-width: 1300px) {
+        flex-direction: column;
+        padding: 2rem 1rem;
+        #logo {
+            display: inline-block;
+            margin: 1rem;
+        }
+        ul {
+            padding: 2rem;
+            justify-content: space-around;
+            width: 100%;
+            li {
+                padding: 0;
             }
         }
     }
-`
+`;
+
+const Line = styled(motion.div)`
+    height: 0.3rem;
+    background-color: var(--d-blueish);
+    width: 0%;
+    position: absolute;
+    bottom: -60%;
+    left: 65%;
+    @media (max-width: 1300px) {
+        left: 0%;
+    }
+`;
+
 export default Nav;
