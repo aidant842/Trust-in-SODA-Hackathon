09075d64871b9351ac839be3590f06a7434b6272
andrius-siup <andrius.siupinys@gmail.com>
2021-09-10 16:42:39 +0000

    Added footer and container

--- space4all/src/components/Footer.js
+++ space4all/src/components/Footer.js
@@ -1,13 +1,39 @@
 import styled from "styled-components"
 // Images
-import logo from "../static/images/logo.png"
+import logoTitle from "../static/images/logo-title.png"
+// Fontawesom
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faGithubSquare} from '@fortawesome/free-brands-svg-icons';
 
 const Footer = () => {
     return (
         <StyledFooter>
             <ul>
                 <li>
-                    <img></img>
+                    <img src={logoTitle}></img>
+                </li>
+                <li className="footerIcon">
+                    <a href=""><FontAwesomeIcon icon={faGithubSquare}/></a>
+                </li>
+                <li>
+                    <ul class="footerLinkedIn">
+                        <li>
+                            <a href="">Aidan</a>
+                        </li>
+                        <li>
+                            <a href="">Allana</a>
+                        </li>
+                        <li>
+                            <a href="">Aoife</a>
+                        </li>
+                        <li>
+                            <a href="">Andrius</a>
+                        </li>
+                        <li>
+                            <a href="">John</a>
+                        </li>
+                    </ul>
+                    
                 </li>
             </ul>
         </StyledFooter>
@@ -16,30 +42,49 @@ const Footer = () => {
 };
 
 const StyledFooter = styled.footer `
+
+    .footerIcon {
+        font-size: 32px;
+        :hover{
+            font-size: 2.3rem;
+        }
+    }
+    .footerLinkedIn{
+        box-shadow: unset;
+        margin-top: 0;
+        li{
+        :hover{
+                font-size: 1.2rem;
+                color: var(--red);
+            }
+        }
+    }
     ul{
         display: flex;
         background-color: white;
         box-shadow: 0 0 5px black;
         list-style: none;
+        margin-top: 1rem;
+       
         li {
             margin: auto;
             img{
-                height: 50px;
-                width: 50px;
+                height: 4rem;
+                width: 16rem;
             }
             a{
             cursor: pointer;
             color: var(--blue);
-            /* text-shadow: 0 0 2px var(--d-blueish); */
+            margin: 5px;
             text-decoration: unset;
             :hover{
-                font-size: 1.2rem;
                 color: var(--red);
             }
+            
         }
         }
         
     }
     
 `
-export default Nav;
+export default Footer;
