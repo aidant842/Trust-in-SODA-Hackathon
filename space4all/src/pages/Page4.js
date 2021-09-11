6248f9d765132d800b3c397655460d4686de3ded
andrius-siup <andrius.siupinys@gmail.com>
2021-09-11 11:28:12 +0000

    Added card layouts

--- space4all/src/pages/Page4.js
+++ space4all/src/pages/Page4.js
@@ -1,15 +1,39 @@
 import React from "react";
 // Components
-import Container from "../components/UI/Container";
+import Image from "../components/Image";
+import TextBlockWithHeader from "../components/TextBlockWithHeader";
+import HR from "../components/UI/HR";
+import ProductCard from "../components/Products/ProductCard";
+import { Products } from "../components/Products/Products";
 
+// Images
+import logo from "../static/images/logo.png";
+
+// Styled Components
+import styled from "styled-components";
 const Page4 = () => {
-    return (
-        <>
-            <Container>
-                <p>Page 4</p>
-            </Container>
+
+    return ( <>
+        <Image imageSrc = {logo}
+        altText = "logo"
+        width = "100%"
+        height = "auto" / >
+        <HR / >
+        <TextWithHeader heading = "Heading 1"
+        text = "paragraph 1" / >
+        {Products.map((product) => {
+            return (
+                <ProductCard url={product.url} imageSrc={product.imageSrc} altText={product.altText} imgWidth={product.imgWidth} imgHeight={product.imgHeight} heading={product.heading} text={product.text} />
+            )
+        })}
         </>
     );
 };
+
+const TextWithHeader = styled(TextBlockWithHeader)
+`
+    text-align: center;
+    width: 100%;
+`;
 
 export default Page4;
