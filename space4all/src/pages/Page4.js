bb9d50505ae4c52a62d9aa003f04d215ccf7ff2b
Allana Bailey <hmabailey@hotmail.com>
2021-09-12 21:10:00 +0000

    add hero image and introduction to page4

--- space4all/src/pages/Page4.js
+++ space4all/src/pages/Page4.js
@@ -8,17 +8,18 @@ import { Products } from "../components/Products/Products";
 
 // Images
 import logo from "../static/images/logo.png";
+import accessories_header from "../static/images/accessories_header.png";
 
 // Styled Components
 import styled from "styled-components";
 const Page4 = () => {
     return (
         <>
-            <Image imageSrc={logo} altText="logo" width="80%" height="auto" />
+            <Image imageSrc={accessories_header} altText="A banner displaying all of the different accessories and products listed in more detail below." width="80%" height="auto" />
             <HR />
             <TextWithHeader
                 heading="RESOURCES AND ACCESSORIES"
-                text="By taking some time to address areas of the workstation and surrounds the benefits to the user and the company are so much."
+                text="By taking some time to address areas of the workstation and surrounds the benefits to the user and the company are so much. The following are a handful of options that are easily implemented and benefit a number of different type of users with varying disabilities from physical to the invisible addressing neurodiversity."
             />
             {Products.map((product, index) => {
                 return (
